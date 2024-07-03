a979c18d8b5f4250081f6e8604756b50b2ae1cbd
Nikita <belka.nik02@gmail.com>
2024-07-03 22:05:39 +0300

    2 task

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,15 @@
 import css from './FriendListItem.module.css';
+import clsx from "clsx" 
 
 const FriendListItem =({name,isOnline,avatar}) =>{
+
+  const isOnlineCss = clsx(css.name, isOnline ? css.online : css.offline)
+
   return (
     <div className={css.containerFriends}>
       <img className={css.avatar} src={avatar} alt="Avatar" width="48"/>
       <p className={css.name}>{name}</p>
-      <p>{isOnline ? "Online" : "Offline"} </p>
+      <p className={isOnlineCss}>{isOnline ? "Online" : "Offline"} </p>
     </div>
   )
 }
